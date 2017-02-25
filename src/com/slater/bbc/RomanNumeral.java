00ab3e438ada9ede488e2a9a26679cf980f0e755
slaterJJ <jackslater@hotmail.com>
2017-02-25 12:26:16 +0000

    Set up Roman numeral class which implements romanNumeralGenerator and test suite

---
+++ src/com/slater/bbc/RomanNumeral.java
@@ -0,0 +1,8 @@
+package com.slater.bbc;
+
+public class RomanNumeral implements RomanNumeralGenerator {
+
+    public String generate (int num) {
+        return "";
+    }
+}
---
+++ test/com/slater/bbc/RomanNumeralTest.java
@@ -0,0 +1,13 @@
+package com.slater.bbc;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class RomanNumeralTest {
+
+    RomanNumeral test = new RomanNumeral();
+    @Test
+    public void romanNumeralGeneratorTest () {
+
+    }
+}
